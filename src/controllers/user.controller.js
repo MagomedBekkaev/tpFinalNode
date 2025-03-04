7c63a3646fc7fa4716e46efc32bf456507290168
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2025-03-04 14:18:23 +0100

    debut signUp

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,7 +11,7 @@ async function listAllUsers(req, res, next) {
     }
 }
 
-const signUp = async (req, res, next) => {
+const signUp = async (req, res) => {
     try {
         const hash = await bcrypt.hash(req.body.password, 10);
         const user = await userService.signUp({ ...req.body, password: hash });
@@ -23,6 +23,28 @@ const signUp = async (req, res, next) => {
         res.status(400).json({error: error.message});
     }
 };
+
+const signIn = async (req, res) => {
+    try {
+        const user = await userService.signIn(req.body.email);
+        
+        if (!user) {
+            throw new Error('User not found');
+        }
+        
+        const isValid = await bcrypt.compare(req.body.password, user.password);
+        
+        if (!isValid) {
+            throw new Error('Invalid password');
+        }
+        
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+        
+        res.status(200).json({ user, token });
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 module.exports = {
     listAllUsers,
